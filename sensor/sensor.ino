82f2287450ca9efd1495e739c278ae9a21aea2ce
Cedric Honnet <honnet@telecom-paristech.org>
2013-08-17 21:31:50 -0700

    Improve the sensor test file.

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -3,21 +3,28 @@ bool led = 0;
 //const float COEF = 0.9;
 
 int sensorValue = 70;
-int delta = 1;
+int delta = 10;
 
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(9600);
+
+  Serial.println("B");
 }
 
 void loop() {
 //  float sensorValue = analogRead(A1);
 //  sensorValue = sensorValue * COEF + oldValue * (1-COEF);
-  if (sensorValue>130 || sensorValue<70)
+  if (sensorValue>130 || sensorValue<70) {
     delta = -delta;
+    Serial.println("E");
+    Serial.println("V12");
+    delay(2000);
+    Serial.println("B");
+  }
 
-  sensorValue += delta; 
-  
+  sensorValue += delta;
+  Serial.print("H");
   Serial.println(sensorValue);
 
   digitalWrite(13, led = !led);
